0d2ed3aa81e0bb0d7ef6e7aef797535a0ba24523
DESKTOP-UR6OLHP\Maisha <maisha.jauernig@gmail.com>
2018-07-22 11:37:21 -0500

    Small change

--- testproject/REST.py
+++ testproject/REST.py
@@ -122,4 +122,6 @@ def takeinput():
 # extractor
 
 if __name__ == "__main__":
+    print("THIS IS A NEW VERSION")
+    DOWNLOAD_FILES_PATH = "D:\\Dropbox\\MB_dev\\PRDE\\testproject\\outputs\\"
     app.run(debug=True)
--- testproject/testproject.py
+++ testproject/testproject.py
@@ -122,4 +122,6 @@ def takeinput():
 # extractor
 
 if __name__ == "__main__":
+    print("THIS IS A NEW VERSION")
+    DOWNLOAD_FILES_PATH = "/home/user/PRDE/testproject/outputs/"
     app.run(debug=True, host="192.168.1.10")
