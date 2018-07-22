87811c415cee5ec46f2704bc778464a67f2d19ff
DESKTOP-UR6OLHP\Maisha <maisha.jauernig@gmail.com>
2018-07-22 13:16:22 -0500

    guid debugging

--- testproject/session_map.py
+++ testproject/session_map.py
@@ -10,17 +10,26 @@ def get_new_guid_for_fs():
     fs = pidf.FilterSystem()
     guid = str(uuid.uuid1())
     session_map[guid] = fs
+    print ("SESSION_MAP: Guid made: " + guid)
     return guid
 
 def get_FilterSystem(guid):
     """return a FilterSystem corresponding to a guid"""
-    answer = session_map[guid]
-    assert(answer, "Guid not found")
+    if guid in session_map.keys():
+        answer = session_map[guid]
+    else:
+        print ("SESSION_MAP: Guid lost: " + guid)
+        return
+
     return answer
 
 def update_FilterSystem(guid, fs):
     """Update the fs associated with a guid"""
-    answer = session_map[guid]
-    assert(answer, "Guid not found")
+    if guid in session_map.keys():
+        answer = session_map[guid]
+    else:
+        print ("SESSION_MAP: Guid lost: " + guid)
+        return
+
     session_map[guid] = fs
 
